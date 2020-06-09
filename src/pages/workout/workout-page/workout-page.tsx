082c0b816a27c8f783f3c8ad6ddc939c533d656c
serapke <m.serapinas@gmail.com>
2020-06-09 23:04:32 +0300

    Add workout page

---
+++ src/pages/workout/workout-page/workout-page.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { connect } from "react-redux";
+import { RouteComponentProps, Link } from "react-router-dom";
+import { ApplicationState } from "Store/index";
+import { getWorkout } from "Services/workout";
+import { Workout } from "Store/types";
+import { Typography, Button, Box } from "@material-ui/core";
+
+interface RouteParams {
+  id: string;
+}
+
+type AllProps = RouteComponentProps<RouteParams>;
+
+const WorkoutPage: React.FunctionComponent<AllProps> = ({ match }) => {
+  const [workout, updateWorkout] = React.useState<Workout>();
+
+  React.useEffect(() => {
+    getWorkout(match.params.id).then((workout) => updateWorkout(workout));
+  }, []);
+
+  if (!workout) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <Box display="flex" alignItems="center" justifyContent="space-between">
+        <Typography variant="h4">{workout.title}</Typography>
+        <Box>
+          <Button color="secondary" component={Link} to={`/workout/${match.params.id}/edit`}>
+            EDIT EXERCISE
+          </Button>
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+const mapStateToProps = ({ form }: ApplicationState) => ({
+  form: form.workout.form,
+  id: form.workout.id,
+});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(WorkoutPage);
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import FavoritesPage from "Pages/favorites";
 import RootPage from "Pages/root";
 import WorkoutCreatePage from "Pages/workout/workout-create-page";
 import WorkoutEditPage from "Pages/workout/workout-edit-page";
+import WorkoutPage from "Pages/workout/workout-page";
 import ExerciseCreatePage from "Pages/exercise/exercise-create-page";
 import ExerciseSelectPage from "Pages/exercise/exercise-select-page";
 
@@ -11,29 +12,33 @@ export default [
     component: RootPage,
     routes: [
       {
-        component: Frontdoor,
-        exact: true,
         path: "/",
+        exact: true,
+        component: Frontdoor,
       },
       {
-        component: FavoritesPage,
         path: "/favorites",
+        component: FavoritesPage,
       },
       {
-        component: WorkoutCreatePage,
         path: "/workout/create",
+        component: WorkoutCreatePage,
       },
       {
+        path: "/workout/:id/edit",
         component: WorkoutEditPage,
+      },
+      {
         path: "/workout/:id",
+        component: WorkoutPage,
       },
       {
-        component: ExerciseSelectPage,
         path: "/exercise/select",
+        component: ExerciseSelectPage,
       },
       {
-        component: ExerciseCreatePage,
         path: "/exercise/create",
+        component: ExerciseCreatePage,
       },
     ],
   },
